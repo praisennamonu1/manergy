3fa85721aff114be05698207ee79df3ce35e0f96
Praise Nnamonu <praise.nnamonu@kibo.school>
2023-05-01 09:25:32 +0100

    initial commit for database

---
+++ models/models.py
@@ -0,0 +1,4 @@
+# This imports the required models and classes
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
